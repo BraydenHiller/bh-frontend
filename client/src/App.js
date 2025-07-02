c31669114608811a5e00413deffaed3af05a83c4
Brayden Hiller <Brayden@arhillers.com>
2025-07-02 11:58:08 -0700

    Fix: Correct App.css import path in LandingPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import LandingPage from './LandingPage';
 import Photographer from './Photographer';
 import ClientLogin from './ClientLogin';
 import ClientGallery from './ClientGallery';
