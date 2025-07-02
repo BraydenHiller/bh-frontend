9c9af55989310464e7821403393222afaa98f56c
Brayden Hiller <Brayden@arhillers.com>
2025-07-02 11:52:45 -0700

    Update: Add all route paths including LandingPage, ClientLogin, and Showcase

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Photographer from './Photographer';
-import Showcase from './Showcase';
-import EditShowcase from './EditShowcase';
-import ClientGallery from './ClientGallery';
 import LandingPage from './pages/LandingPage';
+import Photographer from './Photographer';
+import ClientLogin from './ClientLogin';
+import ClientGallery from './ClientGallery';
+import EditShowcase from './EditShowcase';
+import Showcase from './Showcase';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Photographer />} />
-        <Route path="/showcase" element={<Showcase />} />
-        <Route path="/edit-showcase" element={<EditShowcase />} />
-        <Route path="/gallery/:clientId" element={<ClientGallery />} />
         <Route path="/" element={<LandingPage />} />
+        <Route path="/photographer-login" element={<Photographer />} />
         <Route path="/client-login" element={<ClientLogin />} />
-        <Route path="/photographer" element={<Photographer />} />
+        <Route path="/client-gallery/:id" element={<ClientGallery />} />
+        <Route path="/edit-showcase" element={<EditShowcase />} />
+        <Route path="/showcase" element={<Showcase />} />
       </Routes>
     </Router>
   );
